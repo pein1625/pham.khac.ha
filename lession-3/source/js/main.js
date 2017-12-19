5a0989980ef9c258db8b121f37ef8597ff990489
ha <pein1625@gmail.com>
2017-12-19 08:53:13 +0700

    register page

--- lession-3/source/js/main.js
+++ lession-3/source/js/main.js
@@ -2,4 +2,5 @@
 //=require popper.js/dist/umd/popper.js
 //=require bootstrap/dist/js/bootstrap.js
 //=require owl.carousel2/dist/owl.carousel.js
+//=require jquery-validation/dist/jquery.validate.js
 //=require app.js
