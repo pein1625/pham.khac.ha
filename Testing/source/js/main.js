2c1679adb84efea0536c8ef3aa7b26f2c8a60c38
nguyen.manh.tung <nguyenmanhtung205@gmail.com>
2017-12-26 10:50:05 +0700

    init test

---
+++ Testing/source/js/main.js
@@ -0,0 +1,2 @@
+//=require jquery/dist/jquery.min.js
+//=require zoom.js
---
+++ Testing/source/js/zoom.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  // Try ES6
+  const foo = () => 'This file is demo 5';
+  console.log(foo());
+});
